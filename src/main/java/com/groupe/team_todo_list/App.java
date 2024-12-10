3d6d39da20ee30547869fe2435584b78d4171395
tikamarouane <tikamarouane@gmail.com>
2024-12-10 19:28:04 +0100

    Initialisation du projet

---
+++ src/main/java/com/groupe/team_todo_list/App.java
@@ -0,0 +1,13 @@
+package com.groupe.team_todo_list;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
